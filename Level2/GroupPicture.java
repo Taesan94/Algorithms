3d69d4f770f84d08a2fa8c5e2fa2f1c5faad6c21
NAME <eamil@naver.com>
2020-01-03 21:00:24 +0900

    추가

---
+++ Level2/GroupPicture.java
@@ -0,0 +1,21 @@
+package Programmers.Level2;
+
+public class GroupPicture {
+
+	static String[] characters = {
+			"A","C","F","J","M","N","R","T"
+	};
+	
+	public static void main(String[] args) {
+		
+		for ( int i = 0 ; i < characters.length; i++ ) {
+			
+		}
+		
+		
+	}//main
+	
+	private static void find(int start) {
+		
+	}
+}//class
