91d272f023f197982b52f77d6d14614170ad5dff
NAME <gbgo77@naver.com>
2020-03-29 09:50:33 +0900

    문자열압축.. 프로그래머스 best풀이보고 추가해봄.

--- Level1/CharacterCompress.java
+++ Level1/CharacterCompress.java
@@ -12,7 +12,7 @@ public class CharacterCompress {
 		
 		String word = scan.nextLine();
 
-		int answer = solution(word);
+		int answer = solution2(word);
 
 		System.out.println(" 정답은 ?? " + answer);
 
@@ -69,5 +69,34 @@ public class CharacterCompress {
 		
 		return result.length()+exist;
 	}
+	
+	
+	   public static int solution2(String s) {
+	        int answer = 0;
+
+	        for(int i=1; i<=(s.length()/2)+1; i++){
+	            int result = getSplitedLength(s, i, 1).length();
+	            answer = i==1 ? result : (answer>result?result:answer);
+	        }
+
+	        return answer;
+	    }
+	
+    public static String getSplitedLength(String s, int n, int repeat){
+        if(s.length() < n) return s;
+        String result = "";
+        String preString = s.substring(0, n);
+        String postString = s.substring(n, s.length());
+        
+        System.out.println(" preString : " + preString +", postString : " + postString );
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!postString.startsWith(preString)){
+            if(repeat ==1) return result += preString + getSplitedLength(postString, n, 1);
+            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
+        }
+
+        return result += getSplitedLength(postString, n, repeat+1);
+    }
 	
 }//class
