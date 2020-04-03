3f84d6a88dbd0ff266570cda6437900ec9e24b23
NAME <gbgo77@naver.com>
2020-04-03 15:20:40 +0900

    소수찾기 풀어보기

--- Level2/MakePrime.java
+++ Level2/MakePrime.java
@@ -3,16 +3,34 @@ package Programmers.Level2;
 public class MakePrime {
 
 	public static void main(String[] args) {
+		
+		int[] nums = { 1,2,3,4 };
+		
+		int result = solution(nums);
+	
+		System.out.println("result : " + result );
+	}
 
+	public static int solution(int[] nums) {
+		int answer = 0;
+
+		for ( int i = 0 ; i < nums.length-2; i++ ) {
+			for ( int j = i+1 ; j < nums.length-1; j++ ) {
+				for ( int k = j+1; k < nums.length; k++ ) {
+					int sum =  nums[i]+ nums[j]+ nums[k]; 
+					if(primeCheck(sum)) answer++;
+				}
+			}
+		}
+		
+		return answer;
 	}
 	
-    public int solution(int[] nums) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
-    }
+	private static boolean primeCheck(int sum) {
+		for ( int i = 2; i < sum/2; i++ ) {
+			if( sum % i == 0 ) return false;
+		}
+		return true;
+	}
 
 }
