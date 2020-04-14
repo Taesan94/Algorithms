7ddaa37e9daf7f5644db435027eb360e2bf9f095
NAME <gbgo77@naver.com>
2020-04-14 12:37:05 +0900

    조이스틱 다시풀기 !

--- Level2/JoyStick.java
+++ Level2/JoyStick.java
@@ -4,9 +4,9 @@ public class JoyStick {
 
 	public static void main(String[] args) {
 
-		String number ="BAAAAAA";
+		String number ="JEROEN";
 
-		int answer = solution( number );
+		int answer = solution2( number );
 
 		System.out.println(answer);
 
@@ -71,12 +71,44 @@ public class JoyStick {
 				}else i++;
 			}
 			
-			
 			System.out.println(" 돌아와서 : " + moveCnt);
 			
 			if ( min > moveCnt ) min = moveCnt ;
 		}
 
+		return answer+min;
+	}
+	
+	public static int solution2(String name) {
+		int answer = 0 ;
+		
+		// 위,아래로 알파벳을 움직여주는 경우.명확하다.
+		for ( char c : name.toCharArray()) {
+			if( c < 78 ) answer+=c%65;
+			else answer+=91-c;
+		}
+		
+		// 좌,우의 최소는 문자열 시작부터 끝가지 가는경우이다.
+		int min = name.length()-1;
+		
+		// A가 연속되는 경우에는 뒤로돌아갔을때 최솟값을 구해주어서 비교해준다.
+		for ( int i = 0; i < name.length(); i ++ ) {
+			int index = i;
+			
+			if( name.charAt(index)=='A' ) {
+				while( index < name.length() && name.charAt(index)=='A') {
+					index++;
+				}
+				
+				// 종료시점의 index는 연속된 A가 끝나는지점이다.
+				
+				// ((i-1)*2)은  A가 가기전까지 갔다가, 다시 Back하는 수를 세준것이다.
+				// 연속된 A가 끝나는 index까지 뒤에서 세야함으로 ( 전체길이 - index )를 해준다.
+				int moveCnt = ((i-1)*2) + name.length() - index;
+				min = Math.min(min, moveCnt);
+			}
+		}
+		System.out.println(" 좌,우 이동거리 : " + min );
 		return answer+min;
 	}
 
