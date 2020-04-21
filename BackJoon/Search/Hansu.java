43a64a2c2aa5d8b60e2ae112f22d63f7bcd52225
NAME <gbgo77@naver.com>
2020-04-21 16:40:58 +0900

    한수 풀이 !

--- BackJoon/Search/Hansu.java
+++ BackJoon/Search/Hansu.java
@@ -3,7 +3,43 @@ package Programmers.BackJoon.Search;
 public class Hansu {
 
 	public static void main(String[] args) {
-
+		
+		int result = solution(1000);
+		
+		System.out.println(" result : " + result);
+		
+	}
+	
+	public static int solution(int N) {
+		int answer = 0 ;
+		
+		for(int i = 1 ; i <= N; i ++ ) {
+			
+			if( i < 100 ) answer++;
+			else {
+				if(hansuCheck(i)) answer++;
+			}
+		}
+		return answer;
+	}
+	
+	private static boolean hansuCheck( int i ) {
+		boolean result = true;
+		
+		char[] arr = String.valueOf(i).toCharArray();
+		
+		int minValue = arr[1]-arr[0];
+		
+		
+		for(int j = 1 ; j < arr.length-1; j++) {
+			
+			int minValue2 = arr[j+1]-arr[j];
+			if( minValue != minValue2 ) return false;
+		}
+		
+		System.out.println( " i : " + i );
+		
+		return result;
 	}
 
 }
