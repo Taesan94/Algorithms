d3a6fa7288a3ad01a7990c761aa6b537b04cff07
NAME <gbgo77@naver.com>
2020-04-29 22:24:18 +0900

    4단고음 풀이.... 이거는 패턴을 잘 찾는 문제였다... 어려웠다 ㅠㅠ

--- KaKaoFinal_2017/LittleFriendsGame.java
+++ KaKaoFinal_2017/LittleFriendsGame.java
@@ -1,20 +1,10 @@
 package Programmers.KaKaoFinal_2017;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class LittleFriendsGame {
-
-	private static Map<Integer,List<String>> result = new TreeMap();
-	private static int max = 0 ;
 
 	public static void main(String[] args) {
 
@@ -22,11 +12,9 @@ public class LittleFriendsGame {
 		int n = 3 ;
 
 		String[] board = {
-				//"DBA", 
-				//"C*A", 
-				//"CDB"	
-				"NRYN",
-				"ARYA"
+				"DBA", 
+				"C*A", 
+				"CDB"	
 		};
 
 		String result = solution(m,n,board);
@@ -36,163 +24,163 @@ public class LittleFriendsGame {
 	}
 
 	public static String solution(int m, int n, String[] board) {
-		String answer = "IMPOSSIBLE";
-
-		Set<Character> set = new TreeSet();
-
-		for(String row : board ) {
-			for( char c : row.toCharArray()) {
-				if( c >= 'A' && c <= 'Z') {
-					set.add(c);
-				}
-			}
-		}
-
-		max = set.size();
-
-		int seq = 0;
-		// 최초 수행.
-		search(board,"",seq);
-
-		seq++;
-
-		if( result.size() < 1 ) return answer;
-		else {
-			System.out.println("result : " + result.toString());
-
-			for( int i=0; i<max-1; i++) {
-				
-				List<String> r = result.get(seq-1);
-				
-				for(int j=0; j<r.size(); j++ ) {
-					String word = r.get(j);
-					String[] boardClone = board.clone();
-					
-					for( int k=0; k<word.length(); k++) {
-						breakBoard(boardClone, word.charAt(k));
-					}
-					
-					search(boardClone,word,seq);
-				}
-				
-				if(result.get(seq)==null) {
-					System.out.println("요기");
-					return answer;
-				}
-				System.out.println("result : " + result.toString());
-				seq++;
-			}
-		}
-
-		System.out.println("result : " + result.toString());
 		
-		if(result.get(max-1) != null ) {
-			answer = result.get(max-1).get(0);
-		}
+		String result = checkBreak(board);
+		
+		System.out.println(" result : " + result );
+		
+		String[] r = result.split(",");
+		
+		Arrays.sort(r);
+		System.out.println(Arrays.toString(r));
+		
+		BFS(board,result);
 
-		return answer;
+		// 회전 !
+
+		// ㄱ,ㄴ 모두봐서 가능한 문자열이 있다면  뿌시고, 재귀한다.
+
+		// 이 때 가능한 모든 문자열의 기본 board는 동일해야 한다.
+
+		// 재귀하면서는 변경되어야 한다.
+
+
+
+		return "";
+	}
+	
+	private static String BFS(String[] board, String s) {
+		
+		String result ="";
+		
+		String[] sS = s.split(",");
+		
+		// Queue에 넣는다.
+		Queue<String> q = new LinkedList<String>();
+		
+		for(String r : sS ) {
+			q.add(r);
+		}
+		// 뽑은 대상으로 breakBoard,checkBreak 를 재귀한다.
+		while(q.isEmpty()) {
+			
+			String str = (String)q.poll();
+			result += str ;
+			
+			char c = str.charAt(str.length()-1);
+			
+			String[] cloneBoard = board.clone();
+			breakBoard(cloneBoard,c);
+			String ret = checkBreak(cloneBoard);
+			BFS(cloneBoard,ret);
+			
+		}
+		
+		System.out.println(" resultssst : " + result );
+		
+		return result ;
+		
 	}
 
-	// 현재 board에서 뿌실 수 있는 대상을 찾는다.
+	private static String checkBreak(String[] board) {
 
-	// 가능한 대상이 있다면 뿌시고, 재귀한다.
-
-	// max만큼 문자열이 만들어진다면 String으로 붙여서 List에 넣어준다.
-
-	private static void search(String[] board, String word , int seq ) {
-
-		System.out.println(" word : " + word +", board : " + Arrays.toString(board));
+		String result="";
 
 		for(int i=0; i<board.length; i++) {
-			for(int j=0; j<board[i].length(); j++ ) {
+			String row = board[i];
+			for( int j=0; j<row.length(); j++ ) {
 
+				char tile = row.charAt(j);
 				boolean find = false;
-				char tile = board[i].charAt(j);
-				// 현재 타일이 도달 가능한지 확인해야 한다. ㄱ,ㄴ 두 경우 ..
 
-				if(tile=='.' || tile=='*') continue;
 
-				System.out.println("i : " + i +", j : " + j + ", tile : " + tile );
+				// 뿌실 수 있는 문자열이 아니다.
+				if( tile=='.' || tile =='*') continue;
 
-				// ㄱ
-				int nextCol = j+1;
-				
-				boolean last = false;
-				
-				while( !find && !last && nextCol < board[0].length() ) {
-					char nextTile = board[i].charAt(nextCol);
-					if( nextTile=='.' ) {
-						int nextRow = i+1;
-						while( nextRow < board.length ) {
-							System.out.println(" word : "+ word + " nextRow : " + nextRow +", nextCol : " + nextCol + ", board : " + Arrays.toString(board) );
-							nextTile = board[nextRow].charAt(nextCol);
-							System.out.println(" nextTile : " + nextTile );
-							if(tile==nextTile) {
-								find=true ;
-								break;
-							}
-							else if(nextTile=='.') {
-								nextRow++;
-							}
-							else {
-								nextCol++;
-								break;
-							}
-						}
-						if(nextRow >= board.length-1 ) {
-							last=true;
-						}
-					}else if(tile==nextTile) {
-						find=true;
-						break;
-					}else break;
-				}
-				// ㄴ
-				int nextRow = i+1;
-				
-				last = false ;
-				
-				if(!find) {
-					while( !find && !last && nextRow < board.length ) {
-						char nextTile = board[nextRow].charAt(j);
+				// ㄱ일때를 본다.
+				for ( int r=j+1; r<row.length(); r++ ) {
 
-						if( nextTile=='.' ) {
-							nextCol= j;
-							while( nextCol < board[0].length() ) {
-								nextTile = board[nextRow].charAt(nextCol);
-								if(tile==nextTile) {
-									find=true;
-									break;
-								}
-								else if(nextTile=='.') nextCol++;
-								else {
-									nextRow++;
-									break;
-								}
-							}
-							
-							if(nextCol >= board[0].length()-1 ) {
-								last=true;
-							}
-						}else if(tile==nextTile) {
-							find=true;
-							break;
-						}else break;
+					char nextTile = row.charAt(r);
+
+					// 문자열 비교 수행.
+					if( tile == nextTile ) {
+						find = true;
+						// 같은 경우에는 기록해준다.
+						result+=String.valueOf(tile)+",";
 					}
-				}
 
-				// 찾았으면
-				if(find) {
-					List<String> chars = result.getOrDefault(seq, new ArrayList());
-					chars.add(word+String.valueOf(tile));
-					result.put(seq, chars);
-				}
+					if( !find ) {
+						// 아닌 경우에는 nextTile 뭔지에 따라서 다르다.
+						if( nextTile == '*' ) { // 우측으로 더이상 갈 수 없다.
+							continue;
+						}else if( nextTile == '.') { // 비어있는 경우이다.
+							// 아래로 훑어준다.
+							int down=i+1;
+							while( down < board.length ) {
+								
+								char downTile = board[down].charAt(j);
+								
+								if( tile==downTile ) {
+									find = true;
+									result+=String.valueOf(tile)+",";
+									break;
+								}else if( downTile == '*' ) {
+									// 아래로 더이상 갈 수 없다.
+									// ㄱ을 보는 상황이니 j값을 ++해주고 종료시킨다.
+									break;
+								}else if( downTile == '.' ) {
+									down++;
+								}
+							}
+						}
+					}
+				} // ㄱ for
 
+				if( !find ) {
+					// ㄴ일때를 본다.
+					for ( int s=i+1; s<board.length; s++ ) {
+
+						char nextTile = board[s].charAt(j);
+
+						// 문자열 비교 수행.
+						if( tile == nextTile ) {
+							find = true;
+							result+=String.valueOf(tile)+",";
+						}
+
+						if( !find ){
+							// 아닌 경우에는 nextTile 뭔지에 따라서 다르다.
+							if( nextTile == '*' ) { // 우측으로 더이상 갈 수 없다.
+								continue;
+							}else if( nextTile == '.') { // 비어있는 경우이다.
+								// 오른쪽으로 훑어준다.
+								int right=j+1;
+
+								while( right < row.length() ) {
+									
+									char rightTile = board[s].charAt(right);
+									
+									if( tile==rightTile ) {
+										find = true;
+										result+=String.valueOf(tile)+",";
+										break;
+									}else if( rightTile == '*' ) {
+										// 아래로 더이상 갈 수 없다.
+										// ㄱ을 보는 상황이니 j값을 ++해주고 종료시킨다.
+										break;
+									}else if( rightTile == '.' ) {
+										right++;
+									}
+								}
+							}
+						}
+					} // ㄴ for
+				}// if( !find )
 			}
 		}
-
-		return ;
+		return result;
 	}
+
 
 	private static void breakBoard(String[] board, char breakChar ) {
 
