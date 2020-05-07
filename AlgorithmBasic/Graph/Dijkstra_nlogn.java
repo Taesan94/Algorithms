2fea778c736660ad935cd038af7a8bc6bb3f3619
NAME <gbgo77@naver.com>
2020-05-07 21:57:19 +0900

    가장 먼 노드..그냥 단순한 bfs문제였는데 다익스트라로했다가 시간초과나고.. 작은실수로 삽질했다 ㅠㅠㅠ 좀더 신중해지자...... 파이잉이이이이이팅

--- AlgorithmBasic/Graph/Dijkstra_nlogn.java
+++ AlgorithmBasic/Graph/Dijkstra_nlogn.java
@@ -1,8 +1,6 @@
 package Programmers.AlgorithmBasic.Graph;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Dijkstra_nlogn {
