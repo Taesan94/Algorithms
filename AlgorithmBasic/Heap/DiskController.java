d0658643ad6fea233dacbe13245f11533e4243b1
NAME <gbgo77@naver.com>
2020-05-07 14:34:01 +0900

    이얏.. 디스크컨드롤러 어제 하루종일 못푼던걸 오늘 다시고민하니깐 금방해결했다.. 굿.

--- AlgorithmBasic/Heap/DiskController.java
+++ AlgorithmBasic/Heap/DiskController.java
@@ -36,40 +36,42 @@ public class DiskController {
 	}
 
 	public static int solution(int[][] jobs) {
-		
+
 		// 먼저 들어온 순서대로 입력값을 정렬해준다.
 		Arrays.sort(jobs,(o1,o2)->{
 			return o1[0]-o2[0];
 		});
-		
+
 		PriorityQueue<Process> pq = new PriorityQueue<>();
 
 		// 최초 시간은 가장적은 대기시간 프로세스의 시작시간이다.
 		int sum = 0 ; // 총 소요시간을 기록한다.
-		int time = jobs[0][0] ;
-		
+		int time = jobs[0][0] ; // 걸린시간.
+
 		// 모든 process를 처리하면서,하나의 프로세스가 끝났을 때 다음으로 처리 될 수 있는 프로세스들을add 한다.
 		for( int i=0; i<jobs.length; i++ ) {
-			
+
 			int[] job = jobs[i];
-			
-			// 가장 앞에있는 프로세스의 출발시간이, 현재 진행한 시간보다 큰 경우이다.
+
+			// 현재 소요시간보다 다음 작업의 출발시간이 클 때, PriorityQueue에 작업이 남아 있으면, 
+			// 다음 작업보다 작거나 같을 때 까지는 작업을 처리해도 된다.
 			if( job[0] > time ) {
-				// 가장 앞의 시간이 되기전까지는 순서대로 처리해도 된다.
-				while( !pq.isEmpty() && time+pq.peek().working <= job[0] ) {
-					Process p = pq.poll();
-					System.out.println("##" + p.toString());
-					time += p.working;
-					sum += time-p.start;
-					System.out.println(" time : " + time +" , sum : " + sum );
+				if ( pq.isEmpty() ) time = job[0];
+				else {
+					while( !pq.isEmpty() ) {
+						Process p = pq.poll();
+						time += p.working;
+						sum += time-p.start;
+						// 다음 작업을 넣어도, 우선순위대로 처리 될 수 있을 때 break해준다.
+						if ( time > job[0] ) break;
+					}
 				}
-				time = job[0];
 			}
-			
+
 			int index = i;
 
 			boolean visit = false;
-			
+
 			// 현재시간에 처리가능한 대상을 모두 넣어주기.
 			while( index < jobs.length && jobs[index][0] <= time ) {
 				pq.add(new Process(jobs[index][0],jobs[index][1]));
@@ -78,29 +80,29 @@ public class DiskController {
 			}
 
 			if(visit) i=index-1;
-			
+
 			System.out.println(" 처리 할 수 있는 놈들 " + pq.toString());
-			
+
 			Process p = pq.poll();
-			
+
 			System.out.println(p.toString());
-			
+
 			time += p.working;
 			sum += time-p.start;
-			
+
 			System.out.println(" time : " + time +" , sum : " + sum +", 소요시간 : "+(time-p.start));
-			
+
 		}
-		
+
 		while( !pq.isEmpty() ) {
 			Process p = pq.poll();
-				
+
 			if(p.start > time) time= p.start;
 			System.out.println("###" + p.toString());
 			time += p.working;
 			sum += time-p.start;
 		}
-		
+
 		return sum/jobs.length;
 	}
 
