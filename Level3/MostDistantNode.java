350fd6bbcd39a9a9144f028815c46716ab930884
NAME <gbgo77@naver.com>
2020-05-08 18:27:18 +0900

    자물쇠와 열쇠.. 91점이다 ㅠㅠ 너무오래잡고있었다..

--- Level3/MostDistantNode.java
+++ Level3/MostDistantNode.java
@@ -2,7 +2,6 @@ package Programmers.Level3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
