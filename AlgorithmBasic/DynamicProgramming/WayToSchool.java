fbffc43ea51a5413bfdd4c38b697b1029b22670b
NAME <gbgo77@naver.com>
2020-05-12 20:51:47 +0900

    dp유형 등굣길 풀어보기

---
+++ AlgorithmBasic/DynamicProgramming/WayToSchool.java
@@ -0,0 +1,14 @@
+package Programmers.AlgorithmBasic.DynamicProgramming;
+
+public class WayToSchool {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    public static int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        return answer;
+    }
+
+}
