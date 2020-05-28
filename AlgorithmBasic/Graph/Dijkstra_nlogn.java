deb72f64fddedfa261842327e9326983341a9b36
NAME <gbgo77@naver.com>
2020-05-28 13:38:34 +0900

    다익스트라 다시복습..

--- AlgorithmBasic/Graph/Dijkstra_nlogn.java
+++ AlgorithmBasic/Graph/Dijkstra_nlogn.java
@@ -38,22 +38,25 @@ public class Dijkstra_nlogn {
 			Arrays.fill(weight[i], INF);
 			distance[i] = INF;
 		}
-
+		
+		// 그래프 연결 작업, 양뱡향으로 연결
 		for(int i=0; i<edge.length; i++) 
 			weight[edge[i][0]][edge[i][1]]=weight[edge[i][1]][edge[i][0]]=1;
-		
 		
 		for( int[] w : weight ) {
 			System.out.println(Arrays.toString(w));
 		}
 		
-
+		// 다익스트라 알고리즘은, 하나의 정점에서 모든 정점간의 최단거리를 구한다.
+		// 문제에서 시작 정점은 1이다.
 		dijkstra(1);
-
+		
 		Arrays.sort(distance);
-
+		
+		// 가장 멀리 떨어진 노드를 구하는 문제이기 때문에, 가장 먼 거리를 찾는다.
 		int max = distance[N-1];
 
+		// 큰 대상부터, max와 동일한 거리에 있는 대상들만 count해준다.
 		for(int i=N-1; i>=0; i-- ) {
 			if(distance[i]==max) answer++;
 			else break;
@@ -64,10 +67,15 @@ public class Dijkstra_nlogn {
 
 	static void dijkstra(int start) {
 
-		// 가중치가 가장 작은 대상을 넣어간다. 최초 시작은 start의 정보이다.
-		// 그냥 vertex로 해봤더니.. 거리기준으로 정렬 할 수 가 없다..
+		// 가중치가 가장 작은 대상을 넣어간다.
+		// 작은 대상을 먼저 처리해야하기 때문에, PriorityQueue를 사용한다.
+		
+		// 내가 풀었던 문제에서는 가중치가 모두 1이라, PriorityQueue적용을 하지않아도 해결할 수 있는것 같다..
+		// 하지만, 가중치가 Random할 때, 하나의 정점에 2개가 인접했다면, 가중치가 더 작은 정점을 먼저 방문한 후에 최소 거리를 구하는게 맞다..
+		// 그렇기 때문에, heap구조의 pq를 활용한다.
 		PriorityQueue<Pair> pq = new PriorityQueue<>();
-
+		
+		// 시작 정점의 최단거리는0이다.
 		distance[start] = 0;
 		pq.add(new Pair(start,0));
 
@@ -77,30 +85,37 @@ public class Dijkstra_nlogn {
 			Pair pair = pq.poll();
 
 			int vertex = pair.vertex;
-			int cost = pair.distance;
 			
+			// 해당 정점을 방문했다는 것은, 최단거리를 구했다는 의미이다.
 			if( visit[vertex] ) continue;
 			
 			// 방문처리
 			visit[vertex] = true;
 
 			// 인접한 노드 중에서 최소 값을 갱신 할 수 있다면, 최소비용을 변경해준다.
+			// 모든 정점을 확인한다.
 			for( int i=1; i<N; i++) {
-				if( !visit[i] ) {
-					if( ( weight[vertex][i] + distance[vertex] )< distance[i] ) {
+				if( !visit[i] ) { // 최단거리를 구하지않은 정점을 기준으로 
+					// 1. 해당 정점과 인접한 대상으로 가는 가중치 값 + 해당 정점까지 걸린 최소거리.
+					// 2. 해당 정점의 기록된 최단거리를 비교한다.
+					if( ( weight[vertex][i] + distance[vertex] ) < distance[i] ) { // weight의 초깃 값을 INF로 채웠기 때문에, 연결 되어 있는경우에만 확인하게 된다.
+						// 최소 값을 기록해준다.
 						distance[i] = weight[vertex][i] + distance[vertex];
-						System.out.println("[ vertex : "+vertex+" ], distance["+i+"] : "+ distance[i]);
+						// 기준 정점으로부터, 인접한 정점의 최단경로를 구한 후
+						// 인접한 정점 이후의 인접한 대상을 찾기위하여 add해준다.
 						pq.add(new Pair(i,distance[i]));
 					}
 				}
 			}
 			
-			System.out.println(" ### distance : " + Arrays.toString(distance) + ", " + pair.toString());
+			System.out.println( "[ " + vertex +" ] 에 인접한 정점들은 ??? : " + pq.toString());
+			
 		}
 
-		System.out.println(" distance REsult : " + Arrays.toString(distance));
 	}
-
+	
+	// 해당 정점에 도달 할 수 있는 최단거리를 기록하기위한 클래스이다.
+	// distance는 가중치값이다, 가중치가 작은 값이 먼저 처리되도록 정렬하기 위함이다.
 	static class Pair implements Comparable<Pair>{
 
 		int vertex;
---
+++ AlgorithmBasic/Graph/Rank.java
@@ -0,0 +1,71 @@
+package Programmers.AlgorithmBasic.Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Rank {
+
+	public static void main(String[] args) {
+
+		int n =5 ;
+
+		int[][] results ={
+				{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}
+		};	
+
+		int result = solution(n,results);
+
+		System.out.println( " result : " + result );
+
+	}
+
+	public static int solution(int n, int[][] results) {
+		int answer = 0;
+
+		List<List<Integer>> players = new ArrayList<>();
+		List<List<Integer>> winners = new ArrayList<>();
+
+		for( int i=0; i<=n; i++ ) {
+			players.add(new ArrayList<>());
+			winners.add(new ArrayList<>());
+		}
+
+		for(int[] r : results ) {
+
+			int winner = r[0];
+			int looser = r[1];
+
+			players.get(winner).add(looser);
+			players.get(looser).add(winner);
+			winners.get(looser).add(winner);
+		}
+
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		boolean[] visit = new boolean[n+1];
+
+		// 처음 찾을 수 있는 대상을 확인.
+		for ( int i=1; i<=n; i++ ) {
+			if( players.get(i).size()+1 == n ) {
+				q.add(i);
+				visit[i] = true;
+				answer++;
+			}
+		}
+
+		while ( !q.isEmpty() ) {
+			int ok = q.poll();
+			for ( int i=1; i<=n; i++ ) {
+				
+				List<Integer> list = players.get(i);
+				if( list.contains(ok)) {
+					
+					
+				}
+			}
+		}
+		return answer;
+	}
+}
