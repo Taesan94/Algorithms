705174cea0987eab6d176ddbd479905e7f116218
NAME <gbgo77@naver.com>
2020-06-05 19:23:20 +0900

    Level3 문제, 기지국설치 풀이

---
+++ Level3/Palindrome.java
@@ -0,0 +1,26 @@
+package Programmers.Level3;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		String s = "abcdcba";
+		
+		int result = solution(s);
+		
+		System.out.println("result : " + result );
+		
+		
+	}
+
+	public static int solution(String s){
+		int answer = 0;
+		
+		char[] arr = s.toCharArray();
+		
+		// 
+		
+		
+		return answer;
+	}
+}
