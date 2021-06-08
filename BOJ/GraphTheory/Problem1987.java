065515594aa51a80894424200911fe5161340c64
taesan <pawer02023@gmail.com>
2021-06-08 13:23:08 +0900

    moidfy

--- BOJ/GraphTheory/Problem1987.java
+++ BOJ/GraphTheory/Problem1987.java
@@ -2,9 +2,6 @@ package BOJ.GraphTheory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Problem1987 {
