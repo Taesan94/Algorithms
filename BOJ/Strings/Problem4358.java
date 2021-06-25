1f57cd750408625fd93625777d6663114a78e39f
taesan <pawer02023@gmail.com>
2021-06-25 22:31:14 +0900

    printf -> write

--- BOJ/Strings/Problem4358.java
+++ BOJ/Strings/Problem4358.java
@@ -1,7 +1,9 @@
 package BOJ.Strings;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,6 +13,7 @@ public class Problem4358 {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Map<String, Integer> map = new TreeMap<>();
         String s = "";
@@ -20,12 +23,11 @@ public class Problem4358 {
             map.put(s, map.getOrDefault(s, 0) + 1);
             total += 1.0;
         }
-
         Iterator itr = map.keySet().iterator();
-
         while (itr.hasNext()) {
             String key = (String)itr.next();
-            System.out.printf("%s %.4f\n", key, (map.get(key) / total) * 100);
+            bw.write(String.format("%s %.4f\n", key, (map.get(key) / total) * 100));
         }
+        bw.flush();
     }
 }
